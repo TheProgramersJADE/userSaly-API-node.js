cef079612fda3dd6199f60d569f6dc0022415dc8
emilyflores.093 <emilyflores.093@gmail.com>
2025-09-09 15:08:41 +0000

    Hola desde API USERSALY

--- index.js
+++ index.js
@@ -8,14 +8,10 @@ const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/api', (req, res) => {
-  res.json({"msg": "Hello world"});
-});
+  
+  app.get('/', (req, res) => {
+    res.send('API DE USERSALY!');
+  });
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
